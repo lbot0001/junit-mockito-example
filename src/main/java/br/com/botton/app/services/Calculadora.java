54446c4ad5d5a77d23380e0cd3dabece9d1c9229
Luiz Fernando Botton <lfernbot@emeal.nttdata.com>
2023-04-13 15:49:19 -0300

    TDD Calculadora adicionado

---
+++ src/main/java/br/com/botton/app/services/Calculadora.java
@@ -0,0 +1,13 @@
+package br.com.botton.app.services;
+
+public class Calculadora {
+
+    private int resultado;
+    public int soma(int a, int b) {
+
+        resultado = a + b;
+
+        return resultado;
+
+    }
+}
